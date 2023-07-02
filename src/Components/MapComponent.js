4d552cba8be7285856a7eb8e88562a68943fbf62
unknown <corbinisnotyou@gmail.com>
2023-07-02 06:41:26 -0400

    making the map dynamic

--- src/Components/MapComponent.js
+++ src/Components/MapComponent.js
@@ -1,10 +1,11 @@
-import React, { useEffect, useRef } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import 'ol/ol.css';
 import { Map, View } from 'ol';
 import { fromLonLat } from 'ol/proj';
 import { Feature } from 'ol';
 import { Point } from 'ol/geom';
 import { Icon, Style } from 'ol/style';
+import { transform } from 'ol/proj';
 import TileLayer from 'ol/layer/Tile';
 import VectorLayer from 'ol/layer/Vector';
 import VectorSource from 'ol/source/Vector';
@@ -12,11 +13,64 @@ import OSM from 'ol/source/OSM';
 
 const MapComponent = () => {
   const mapRef = useRef(null);
+  const [markers, setMarkers] = useState([])
 
   useEffect(() => {
+
+
+    let newMarkers = [];
+    const url = "https://corbinrose.com/blog/wp-json/wp/v2/posts/?categories=313&per_page=100&_embed";
+                                
+      fetch(url)
+      .then(response => {
+          if (!response.ok) {
+            throw new Error("HTTP error " + response.status);
+          }
+          return response.json();
+      })
+      .then(json => {
+              
+          console.log(json);
+
+          const data= json;
+          console.log("getting new data", data)
+          data.forEach(function(el){
+              const title = el.title.rendered;
+              const desc = el.excerpt.rendered;
+              const linky = el.guid.rendered;
+              const content = "";
+              const featured = el.jetpack_featured_media_url;
+              const latlong = el.acf.latlong.split(",");
+              const lat = latlong[0]
+              const lng = latlong[1];
+              
+              const obj = {
+                  lat: lat,
+                  lng: lng,
+                  name: title,
+                  desc: desc,
+                  guid: linky,
+                  id: el.id,
+                  featured:featured
+              }
+              newMarkers.push(obj)
+          
+              
+          });
+          console.log("making", newMarkers);
+          setMarkers(newMarkers);
+          //corbinrose.maps.makeDatMap(newMarkers, target, 2); 
+    })
+
+  }, []);
+
+  useEffect(()=>{
+
+
+    const viewCenter = (markers[0]? [markers[0].lng, markers[0].lat]:  [0,0]);
     const map = new Map({
       target: mapRef.current,
-
+      projection: 'EPSG:4326',
         layers: [
           new TileLayer({
             source: new OSM(), // OpenStreetMap as the tile layer source
@@ -24,7 +78,7 @@ const MapComponent = () => {
         ],
       
       view: new View({
-        center: fromLonLat([0, 0]),
+        center: fromLonLat(viewCenter),
         zoom: 2,
       }),
     });
@@ -45,12 +99,8 @@ const MapComponent = () => {
     });
 
     // Add markers to the layer
-    const markerCoordinates = [
-      [0, 0],
-      [10, 10],
-      [-10, -10],
-    ];
-
+   
+    /*
     markerCoordinates.forEach((coordinate) => {
       const marker = new Feature({
         geometry: new Point(fromLonLat(coordinate)),
@@ -58,15 +108,80 @@ const MapComponent = () => {
       marker.setStyle(markerStyle);
       markerLayer.getSource().addFeature(marker);
     });
+    */
 
-    // Add the marker layer to the map
-    map.addLayer(markerLayer);
+    markers.forEach((item) => {
+
+      const coord = [item.lng, item.lat];
+      const transformedCoord = transform(coord, 'EPSG:4326', 'EPSG:3857');
+      const point = new Point(transformedCoord);
+      //get image here also?
+
+      const newmarker = new Feature({
+        geometry: point,
+        id: item.id, 
+        description: item.desc, 
+        name: item.name,
+        featured:item.featured
+
+        
+      });
+      newmarker.setStyle(markerStyle);
+      markerLayer.getSource().addFeature(newmarker);
+    });
+
+     // Add the marker layer to the map
+     map.addLayer(markerLayer);
+
+     
+    /*
+    let features = [];
+    markers.forEach(item=>{
+
+      const longitude = item.lng;
+      const latitude = item.lat;
+      
+
+      
+      const iconFeature = new Feature({
+        //geometry: new ol.geom.Point(window.ol.proj.transform([longitude, latitude], 'EPSG:4326', 'EPSG:3857'))
+        geometry: new Point(fromLonLat([longitude, latitude])),
+      });
+      
+
+      const iconStyle = new Style({
+        image: new Icon(({
+          anchor: [0.5, 1],
+          src: "https://corbinrose.com/blog/wp-content/uploads/pin.png"
+        }))
+      });
+      //console.log(item.lng, item.lat)
+      const id = 'icon_' + item.guid;
+      iconFeature.setStyle(iconStyle);
+      iconFeature.setProperties({ 'id': id, 'description': item.desc, 'name': item.name })
+      features.push(iconFeature);
+    })
+    console.log("features", features)
+    var vectorSource = new VectorLayer({
+      features: features
+    });
+
+    var vectorLayer = new VectorLayer({
+      source: vectorSource
+    });
+    map.addLayer(vectorLayer);
+    */
+    
+
+   
 
     // Handle click events on markers
     map.on('click', (event) => {
       map.forEachFeatureAtPixel(event.pixel, (feature) => {
         // Perform actions when a marker is clicked
-        console.log('Marker clicked:', feature.getProperties());
+        const props = feature.getProperties();
+        alert(props.name)
+        console.log('Marker clicked:', props);
       });
     });
 
@@ -74,9 +189,17 @@ const MapComponent = () => {
     return () => {
       map.setTarget(null);
     };
-  }, []);
 
-  return <div ref={mapRef} className="map-container" />;
+  }, [markers])
+
+  return (
+    <div className="map-area">
+      <div ref={mapRef} className="map-container" />
+      <div className="map-preview">
+        Load preview
+      </div>
+    </div>
+  );
 };
 
 export default MapComponent;
